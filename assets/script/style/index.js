e35837eb4763ac75a5f70004a5c0bb88d3cdcbd9
saitovaan35 <saitovaan35@gmail.com>
2025-01-27 23:33:07 +0800

    complete

---
+++ assets/script/style/index.js
@@ -0,0 +1,10 @@
+let time;
+time = 34;
+console.log(time);
+console.log('Старт поездки. Осталось минут: ' + time);
+time = 19;
+console.log('Немного сторис в соцсетях. Осталось минут: '+ time);
+time = 9;
+console.log('Немного не новостей, но событий. Осталось минут: '+ time);
+time = 0;
+console.log(`Вы приехали. Добро пожаловать в Москву.`+ time);
